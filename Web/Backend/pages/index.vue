06791cf1c83fe24dd5ccf418ef4d02de8a406558
Pachara Chantawong <pachara2202@gmail.com>
2021-04-01 13:52:38 +0700

    [Web] Initial Nuxt
    Initial Nuxt project with sample

---
+++ Web/Backend/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="pages">
+    CapyBara
+  </div>
+</template>
+<script>
+export default {
+  layout: 'default'
+}
+</script>
+
+<style scoped>
+.pages {
+  margin: 20px 20px;
+  background: red;
+}
+</style>
