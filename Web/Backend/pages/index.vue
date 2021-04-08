d6aa872b6b2eb3c19b95c37efe0e53be38404d7f
Pachara Chantawong <pachara2202@gmail.com>
2021-04-08 14:44:43 +0700

    [Frontend] Fix $router.replace error when back from register page

--- Web/Backend/pages/index.vue
+++ Web/Backend/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="child-center text-center container">
     <div>
-      <a-button type="primary" block @click="$router.replace('/login')">
+      <a-button type="primary" block @click="$router.push('/login')">
         Sign in
       </a-button>
-      <a-button class="mt-1" block @click="$router.replace('/register')">
+      <a-button class="mt-1" block @click="$router.push('/register')">
         Sign up
       </a-button>
     </div>
