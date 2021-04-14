20b149aeff742e9dee2786a4299ba9576bcf459d
Pachara Chantawong <pachara2202@gmail.com>
2021-04-15 02:37:44 +0700

    move local css class to public css class

--- Web/Backend/pages/index.vue
+++ Web/Backend/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="child-center text-center container">
     <div>
-      <button class="button" @click="$router.push('/login')">
+      <button class="thspp-button" @click="$router.push('/login')">
         Sign in
       </button>
-      <button class="mt-1 button" @click="$router.push('/register')">
+      <button class="mt-1 thspp-button" @click="$router.push('/register')">
         Sign up
       </button>
     </div>
@@ -19,29 +19,6 @@ export default {
     return {
       title: 'Banking App'
     }
-  },
-  data () {
-    return ({
-      user: this.$auth.user
-    })
-  },
-  mounted () {
-    console.log(this.user)
   }
 }
 </script>
-
-<style scoped>
-.button {
-  height: 60px;
-  width: 240px;
-  border-radius: 30px;
-  background-color: #1D3557;
-  color: #fff;
-  font-size: 35px;
-}
-.button:focus {
-  outline: none;
-  box-shadow: none;
-}
-</style>
