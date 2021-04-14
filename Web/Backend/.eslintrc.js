8e1522312e5b506f032030260d0f318b5f01f370
Pachara Chantawong <pachara2202@gmail.com>
2021-04-15 02:27:48 +0700

    Update .eslintrc.js

--- Web/Backend/.eslintrc.js
+++ Web/Backend/.eslintrc.js
@@ -6,20 +6,22 @@ module.exports = {
   },
   parserOptions: {
     parser: 'babel-eslint',
-      "rules":{
-        "indent": [2, "tab"],
-        "no-tabs": 0,
-        "trailing-comma": [true, {
-          "singleline": "never",
-          "multiline": {
-            "objects": "never", 
-            "arrays": "always",
-            "functions": "never",
-            "typeLiterals": "ignore"
-          }
-        }],
-        'no-console': 'off',
-        'eqeqeq': false
+    "rules":{
+      "indent": [2, "tab"],
+      "no-tabs": 0,
+      "trailing-comma": [true, {
+        "singleline": "never",
+        "multiline": {
+          "objects": "never", 
+          "arrays": "always",
+          "functions": "never",
+          "typeLiterals": "ignore"
+        }
+      }],
+      'no-console': 'off',
+      'eqeqeq': false,
+      'vue/singleline-html-element-content-newline': 'off',
+      'vue/multiline-html-element-content-newline': 'off',
     }
   },
   extends: [
