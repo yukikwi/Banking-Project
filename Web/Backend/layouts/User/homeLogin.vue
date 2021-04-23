dc0a5d86dc486bc1a25a6cc6bbe2a050e72c2c5f
Pachara Chantawong <pachara2202@gmail.com>
2021-04-24 00:48:16 +0700

    Optimize footer size on homelayout

--- Web/Backend/layouts/User/homeLogin.vue
+++ Web/Backend/layouts/User/homeLogin.vue
@@ -1,16 +1,16 @@
 <template>
-  <div>
+  <div class="background-main h-100vh">
     <Nuxt />
     <Footer :rounded="false" :container="false" bg-color="transparent" border sticky>
       <div>
-        <div class="home text-center" :style="{ fontSize: '32px', zIndex: 999}" @click="$router.push('/home')">
+        <div class="home text-center" :style="{ fontSize: '40px', zIndex: 999}" @click="$router.push('/home')">
           <a-icon type="home" />
         </div>
         <a-row class="bordered background-white" type="flex" justify="space-between">
-          <a-col class="text-center" :span="12" :style="{ fontSize: '25px' }">
+          <a-col class="text-center" :span="12" :style="{ fontSize: '30px' }">
             <a-icon type="credit-card" />
           </a-col>
-          <a-col class="text-center" :span="12" :style="{ fontSize: '25px' }" @click="$router.push('/home/profile')">
+          <a-col class="text-center" :span="12" :style="{ fontSize: '30px' }" @click="$router.push('/home/profile')">
             <a-icon type="user" />
           </a-col>
         </a-row>
@@ -20,15 +20,20 @@
 </template>
 
 <style scoped>
+.h-100vh{
+  min-height: 100vh;
+}
 .home {
   width: fit-content;
   position: relative;
   left: 50%;
-  bottom: -43px;
+  bottom: -58px;
   transform: translateX(-50%);
   border: #aaa 1px solid;
   border-radius: 50%;
-  width: 50px;
+  width: 80px;
+  height: 80px;
+  line-height: 80px;
   background: #fff;
 }
 .bordered {
