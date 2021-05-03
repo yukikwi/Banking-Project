15e92ddbfb5cc78aef8174f270225c8b4db1a0b2
Pachara Chantawong <pachara2202@gmail.com>
2021-05-03 23:24:39 +0700

    New HomeHeader components

---
+++ Web/Backend/components/Home/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container text-white text-large width-100p">
+    <a-row>
+      <a-col :span="12" @click.native="$router.back()">
+        <a-icon type="left" />
+      </a-col>
+      <a-col class="text-right" :span="12">
+        {{ title }}
+      </a-col>
+    </a-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      default: '',
+      type: String
+    }
+  }
+}
+</script>
