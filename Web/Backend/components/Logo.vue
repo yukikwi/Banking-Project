38d56c1216b92e79dd70a858e5de4a18fede12e0
THSpp <74483748+THSpp@users.noreply.github.com>
2021-05-05 22:19:58 +0700

    Update bank logo

--- Web/Backend/components/Logo.vue
+++ Web/Backend/components/Logo.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
-    <img v-if="size == 'small'" class="img-fluid img-rounded size-50-50" src="~/assets/img/logo.jpg">
-    <img v-if="size == 'normal'" class="img-fluid img-rounded size-100-100" src="~/assets/img/logo.jpg">
-    <img v-if="size == 'large'" class="img-fluid img-rounded size-150-150" src="~/assets/img/logo.jpg">
+    <img v-if="size == 'small'" class="img-fluid img-rounded size-50-50" src="~/assets/img/kai.png">
+    <img v-if="size == 'normal'" class="img-fluid img-rounded size-100-100" src="~/assets/img/kai.png">
+    <img v-if="size == 'large'" class="img-fluid img-rounded size-150-150" src="~/assets/img/kai.png">
   </div>
 </template>
 
