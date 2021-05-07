f08acff7bb18528cf2fffb605660162d1df55202
Pachara Chantawong <pachara2202@gmail.com>
2021-05-08 00:30:08 +0700

    Initial transfer input page

--- Web/Backend/api/bank/index.js
+++ Web/Backend/api/bank/index.js
@@ -23,4 +23,25 @@ router.get('/list', async (req, res) => {
   res.json(result)
 })
 
+router.get('/info/:shortname', async (req, res) => {
+  let result = {}
+
+  try{
+    const bankdata = await db.query('SELECT External_BankShortName shortname, External_BankName title FROM ExternalBank WHERE External_BankShortName = ?', [req.params.shortname])
+    result = {
+      status: 200,
+      data: bankdata[0]
+    }
+  }
+  catch(err){
+    console.log(err)
+    result = {
+        status: 500,
+        comment: "mysql error"
+    }
+  }
+
+  res.json(result)
+})
+
 module.exports = router
