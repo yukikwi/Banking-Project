313b49b0a6f20fb6f84e62d67f3868a7b94d0f7b
Pachara Chantawong <pachara2202@gmail.com>
2021-05-11 20:41:51 +0700

    Final transfer and slip

---
+++ Web/Backend/pages/home/debitcard/_card/transfer/select/_bank/confirm.vue
@@ -0,0 +1,168 @@
+<template>
+  <div>
+    <HomeHeader title="Review Transfer" />
+
+    <div class="rounded-m container background-white pb-80px mb-2">
+      <h2 class="text-muted text-center">
+        Please check transaction details
+      </h2>
+      <a-divider class="boder-main" />
+
+      <div v-if="loading" class="spinner">
+        <a-spin>
+          <a-icon slot="indicator" type="loading" style="font-size: 36px" spin />
+        </a-spin>
+      </div>
+      <div v-else>
+        <a-row>
+          <a-col :span="4">
+            <span class="text-bold text-medium">From</span>
+          </a-col>
+          <a-col :span="3">
+            <a-avatar
+              :src="require(`~/assets/img/bank/brb.png`)"
+            />
+          </a-col>
+          <a-col :span="17">
+            <span class="text-muted">Saving account</span>
+            <h2 class="text-bold mb-0">
+              {{ $route.params.card }}
+            </h2>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="4">
+            <span class="text-bold text-medium">To</span>
+          </a-col>
+          <a-col :span="3">
+            <a-avatar
+              :src="require(`~/assets/img/bank/${$route.params.bank.toLowerCase().trim()}.png`)"
+            />
+          </a-col>
+          <a-col :span="17">
+            <span class="text-muted">{{ bankdata.title }}</span>
+            <h2 class="text-bold mb-0">
+              {{ $store.state.transaction.account_no }}
+            </h2>
+          </a-col>
+        </a-row>
+
+        <a-divider class="background-main" />
+
+        <a-row>
+          <a-col :span="7">
+            <span class="text-bold text-medium">Amount</span>
+          </a-col>
+          <a-col class="text-right" :span="17">
+            <span class="text-large text-bold">{{ $store.state.transaction.amount }} THB</span>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="7">
+            <span class="text-bold text-medium">Fee</span>
+          </a-col>
+          <a-col class="text-right" :span="17">
+            <span class="text-large text-bold">{{ fee }} THB</span>
+          </a-col>
+        </a-row>
+
+        <a-divider class="background-main" />
+        <a-row>
+          <a-col :span="7">
+            <span class="text-bold text-medium">Note</span>
+          </a-col>
+        </a-row>
+        <div class="note mt-1 p-1">
+          {{ $store.state.transaction.note }}
+        </div>
+
+        <button class="mt-1 v-center thspp-button" @click="submit">
+          Confirm
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'User/homeLogin',
+  middleware: ['auth', 'isuserapprove', 'is_debitcard_exist', 'validate_transaction'],
+  data () {
+    return {
+      loading: true,
+      carddata: {},
+      bankdata: {},
+      fee: 0,
+      mode: ''
+    }
+  },
+  async mounted () {
+    const carddata = await this.$axios.post('api/user/debitcard/info', {
+      card_id: this.$route.params.card
+    })
+    if (carddata.data.status === 200) {
+      this.carddata = carddata.data
+      this.fee = this.$store.state.transaction.amount * (this.carddata.data.Interest_Rate / 100)
+      this.loading = false
+    } else {
+      this.$router.replace('/home')
+    }
+
+    if (this.$route.params.bank !== 'BRB') {
+      const bankdata = await this.$axios.get('api/bank/info/' + this.$route.params.bank)
+      if (bankdata.data.status === 200) {
+        this.bankdata = bankdata.data
+        this.mode = 'external'
+      } else {
+        this.$router.replace('/home/debitcard/' + this.$route.params.card + '/transfer/select/' + this.$route.params.bank)
+      }
+    } else {
+      this.bankdata = {
+        title: 'Bara Bank'
+      }
+      this.mode = 'internal'
+    }
+  },
+  methods: {
+    async submit () {
+      // API
+      const result = await this.$axios.post('api/user/debitcard/transfer', {
+        mode: this.mode,
+        sender_addr: this.$route.params.card,
+        target_addr: this.$store.state.transaction.account_no,
+        amount: this.$store.state.transaction.amount,
+        note: this.$store.state.transaction.note,
+        target_bank: this.bankdata.data.id
+      })
+      if (result.data.status === 200) {
+        this.$router.push('/home/debitcard/' + this.$route.params.card + '/transfer/slip/' + result.data.data.insertId)
+      } else if (result.data.status === 404) {
+        this.$router.push('/police')
+      } else {
+        this.$router.push('/contact/sql_err')
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.text-muted{
+  color: #717171;
+}
+.spinner{
+  width: fit-content;
+  margin: auto;
+}
+.note{
+  border: var(--main-color) 2px solid;
+  border-radius: 10px;
+  min-height: 250px;
+}
+.v-center{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ Web/Backend/pages/home/debitcard/_card/transfer/slip/_id.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="flex height-100vh">
+    <div>
+      <div class="rounded-m background-white slip">
+        <div class="center background-main-lite p-1">
+          <a-avatar
+            :src="require(`~/assets/img/bank/brb.png`)"
+          />
+          <span class="ml-1 text-bold">Bara Bank</span>
+        </div>
+
+        <div v-if="loading" class="spinner">
+          <a-spin>
+            <a-icon slot="indicator" type="loading" style="font-size: 36px" spin />
+          </a-spin>
+        </div>
+        <div v-else>
+          <div class="text-center mt-2">
+            <a-icon type="check-circle" theme="twoTone" two-tone-color="#52c41a" :style="{ fontSize: '50px' }" />
+            <br>
+            <span class="text-large text-bold">Transaction Successful</span>
+            <br>
+            <span class="text-medium text-muted">{{ $moment(slipdata.data.Trans_DateTime).format('Do MMM YY, HH:mm') }}</span>
+            <br>
+            <br>
+            <span class="text-large text-muted">Amount</span>
+            <br>
+            <span class="text-large text-bold">{{ slipdata.data.Trans_Amount.toLocaleString() }}</span>
+            <span class="text-large">THB</span>
+          </div>
+          <a-divider class="background-main" />
+          <div class="p-1">
+            <a-row>
+              <a-col :span="4">
+                <span class="text-bold text-medium">From</span>
+              </a-col>
+              <a-col :span="3">
+                <a-avatar
+                  v-if="slipdata.data.User_Sender_Internal_AccountID !== null"
+                  :src="require(`~/assets/img/bank/brb.png`)"
+                />
+                <a-avatar
+                  v-else-if="slipdata.data.User_Sender_External_AccountID !== null"
+                  :src="require(`~/assets/img/bank/${slipdata.data.External_BankShortName}.png`)"
+                />
+                <a-avatar v-else>
+                  Unknow
+                </a-avatar>
+              </a-col>
+              <a-col :span="17">
+                <span class="text-muted">Saving account</span>
+                <h2 v-if="slipdata.data.User_Sender_Internal_AccountID !== null" class="text-bold mb-0">
+                  {{ slipdata.data.User_Sender_Internal_AccountID }}
+                </h2>
+                <h2 v-else-if="slipdata.data.User_Sender_External_AccountID !== null" class="text-bold mb-0">
+                  {{ slipdata.data.User_Sender_External_AccountID }}
+                </h2>
+                <h2 v-else class="text-bold mb-0">
+                  Unknow
+                </h2>
+              </a-col>
+            </a-row>
+            <a-row>
+              <a-col :span="4">
+                <span class="text-bold text-medium">To</span>
+              </a-col>
+              <a-col :span="3">
+                <a-avatar
+                  v-if="slipdata.data.User_Target_Internal_AccountID !== null"
+                  :src="require(`~/assets/img/bank/brb.png`)"
+                />
+                <a-avatar
+                  v-else-if="slipdata.data.User_Target_External_AccountID !== null"
+                  :src="require(`~/assets/img/bank/${slipdata.data.External_BankShortName.toLowerCase()}.png`)"
+                />
+                <a-avatar v-else>
+                  Unknow
+                </a-avatar>
+              </a-col>
+              <a-col :span="17">
+                <span v-if="slipdata.data.User_Target_Internal_AccountID !== null" class="text-muted">Bara Bank</span>
+                <span v-else class="text-muted">{{ slipdata.data.External_BankName }}</span>
+                <h2 v-if="slipdata.data.User_Target_Internal_AccountID !== null" class="text-bold mb-0">
+                  {{ slipdata.data.User_Target_Internal_AccountID }}
+                </h2>
+                <h2 v-else-if="slipdata.data.User_Target_External_AccountID !== null" class="text-bold mb-0">
+                  {{ slipdata.data.User_Target_External_AccountID }}
+                </h2>
+                <h2 v-else class="text-bold mb-0">
+                  Unknow
+                </h2>
+              </a-col>
+            </a-row>
+          </div>
+          <a-divider class="background-main" />
+          <div class="p-1">
+            <a-row>
+              <a-col :span="7">
+                <span class="text-bold text-medium">Fee</span>
+              </a-col>
+              <a-col class="text-right" :span="17">
+                <span class="text-large">{{ slipdata.data.Trans_Fee }} THB</span>
+              </a-col>
+            </a-row>
+            <a-row>
+              <a-col :span="7">
+                <span class="text-bold text-medium">Note</span>
+              </a-col>
+              <a-col class="text-right" :span="17">
+                <span class="text-large">{{ slipdata.data.Trans_Note }}</span>
+              </a-col>
+            </a-row>
+          </div>
+        </div>
+      </div>
+      <button class="mt-1 thspp-button" @click="$router.push('/home')">
+        Finish
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'User/creditcard',
+  middleware: ['auth', 'isuserapprove', 'is_debitcard_exist'],
+  data () {
+    return {
+      slipdata: {},
+      loading: true
+    }
+  },
+  async mounted () {
+    const slipdata = await this.$axios.post('api/user/debitcard/slip', {
+      transaction: this.$route.params.id
+    })
+    if (slipdata.data.status === 200) {
+      this.slipdata = slipdata.data
+      this.loading = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.slip{
+  width: 80%;
+  margin: auto;
+}
+.center{
+  width: 100%;
+  text-align: center;
+}
+.flex{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.height-100vh{
+  min-height: 100vh;
+}
+.spinner{
+  width: fit-content;
+  margin: auto;
+}
+.thspp-button {
+    margin-left: auto;
+    margin-right: auto;
+    display: block;
+}
+</style>
