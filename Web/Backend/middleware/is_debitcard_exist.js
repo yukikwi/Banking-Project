c3ca356e9dd353b55a07881429d78a41e05379f1
Pachara Chantawong <pachara2202@gmail.com>
2021-05-11 20:43:08 +0700

    Cleanup and update css

--- Web/Backend/middleware/is_debitcard_exist.js
+++ Web/Backend/middleware/is_debitcard_exist.js
@@ -2,7 +2,6 @@ export default function ({ $axios, context, route, store, redirect }) {
   return $axios.post('api/user/debitcard/exist', {
     card_id: route.params.card
   }).then((response) => {
-    console.log(response.data.status)
     if (response.data.status !== true) {
       redirect('/home')
     }
